5398a4b53d031e52c649dc35ce97360809bb2c47
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2023-07-08 03:52:16 +0200

    v1.0

--- commons/src/main/kotlin/com/simplemobiletools/commons/extensions/Context.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/extensions/Context.kt
@@ -444,18 +444,15 @@ fun Context.getUriMimeType(path: String, newUri: Uri): String {
     return mimeType
 }
 
-fun Context.isThankYouInstalled() = isPackageInstalled("com.simplemobiletools.thankyou")
+fun Context.isThankYouInstalled():
+    Boolean {
+
+    return true
+}
 
 fun Context.isOrWasThankYouInstalled(): Boolean {
-    return when {
-        resources.getBoolean(R.bool.pretend_thank_you_installed) -> true
-        baseConfig.hadThankYouInstalled -> true
-        isThankYouInstalled() -> {
-            baseConfig.hadThankYouInstalled = true
-            true
-        }
-        else -> false
-    }
+    return true
+
 }
 
 fun Context.isAProApp() = packageName.startsWith("com.simplemobiletools.") && packageName.removeSuffix(".debug").endsWith(".pro")
